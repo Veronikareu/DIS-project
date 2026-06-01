d34cae81b596349d687a174826c57cfe5dd42458
Veronikareu <veronikareusch@gmail.com>
2026-06-01 13:18:43 +0200

    added initial database schema for CVE, vendors, products and vendor_product tables

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE cve (
+    id VARCHAR PRIMARY KEY,
+    mod_date DATE,
+    pub_date DATE,
+    cvss FLOAT,
+    cwe_code VARCHAR,
+    cwe_name VARCHAR,
+    summary TEXT,
+    access_authentication VARCHAR,
+    access_complexity VARCHAR,
+    access_vector VARCHAR,
+    impact_availability VARCHAR,
+    impact_confidentiality VARCHAR,
+    impact_integrity VARCHAR
+);
+
+CREATE TABLE vendors (
+    id VARCHAR PRIMARY KEY,
+    vendor VARCHAR
+);
+
+CREATE TABLE products (
+    cve_id VARCHAR REFERENCES cve(id),
+    vulnerable_product VARCHAR
+);
+
+CREATE TABLE vendor_product (
+    id INTEGER PRIMARY KEY,
+    vendor VARCHAR,
+    product VARCHAR
+);
